7e21eed2e9e6a7ab23ec2688c89d1343ed888c43
enzodallcolle <edallcolle@gmail.com>
2016-06-12 21:03:56 -0300

    Add files via upload

--- js/calculos.js
+++ js/calculos.js
@@ -1,35 +1,35 @@
-/**
- * Created by Enzo on 10/06/2016.
- */
-var peso = document.getElementById("input-peso-figura").value;
-var angulo = document.getElementById("input-angulo").value;
-var distancia = document.getElementById("input-distancia").value;
-var tiempo = document.getElementById("input-tiempo").value;
-var calculos = document.getElementById("select-calculos").value; 
-var resultado = document.getElementById("resultado").value;
-var coefricc = 0;
-
-
-//ACELERACION= ((2 * document.getElementById("input-distancia").value) / Math.pow(document.getElementById("input-tiempo").value));
-
-
 function calcular() {
     //Coeficiente de fricción entre la figura y el plano
-    if(document.getElementById("select-calculos").value == 1)
-    {
-        document.getElementById("resultado").value = document.getElementById("input-peso-figura").value * 9.8 * Math.sin(document.getElementById("input-angulo").value);
+    if(document.getElementById("select-calculos").value == 1) {
+        var resultado=document.getElementById("resultado").value;
+        var anguloxd=document.getElementById("input-angulo").value;
+        var radianes=anguloxd*Math.PI/180;
+        document.getElementById("resultado").value=document.getElementById("input-peso-figura").value * 9.8 * Math.sin(radianes) + " Newton";
     }
 
     //Fuerza de fricción que actua sobre la figura
     if (document.getElementById("select-calculos").value == 2) {
-        
+
+        var anguloxd2=document.getElementById("input-angulo").value;
+        var radianes2=anguloxd2*Math.PI/180;
+        var N=document.getElementById("input-peso-figura").value*9.8*Math.cos(radianes2);
+        var coefriccion=document.getElementById("input-peso-figura").value * 9.8 * Math.sin(radianes2);
+        var aceleracion=(2*document.getElementById("input-distancia").value) / (document.getElementById("input-tiempo").value*document.getElementById("input-tiempo").value);
+
+        document.getElementById("resultado").value=(coefriccion - (document.getElementById("input-peso-figura").value * aceleracion)) / N +" Newton";
     }
 
     //Rapidéz del bloque despues de que se ha desplazado la distancia
     if(document.getElementById("select-calculos").value == 3)
     {
-        alert("asdasd");
+        var aceleracion2=(2*document.getElementById("input-distancia").value) / (document.getElementById("input-tiempo").value*document.getElementById("input-tiempo").value);
+
+        document.getElementById("resultado").value= aceleracion2*document.getElementById("input-tiempo").value + " m/s";
     }
 
 }
+
+
+
+
 
